c32cf594c1ffc091a083b15f94b7ba5315166fc0
Gabriel Santos Rezende <gabrielthel9@gmail.com>
2026-07-12 13:38:46 -0300

    feat: enhance menu screen visual

--- game/menu.lua
+++ game/menu.lua
@@ -21,16 +21,20 @@ function menu:update(dt)
 end
 
 function menu:draw()
-    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.setColor(1, 1, 0, 1)
     love.graphics.setNewFont(48)
     love.graphics.print("Clap Bird", love.graphics.getWidth() / 2 - 140, 80)
 
     love.graphics.setNewFont(24)
-    love.graphics.rectangle("line", self.startButtonX, self.buttonY, self.buttonWidth, self.buttonHeight)
+    love.graphics.rectangle("fill", self.startButtonX, self.buttonY, self.buttonWidth, self.buttonHeight)
+    love.graphics.setColor(0, 0, 0, 1)
     love.graphics.print("Start", self.startButtonX + 48, self.buttonY + 12)
 
-    love.graphics.rectangle("line", self.quitButtonX, self.buttonY + 70, self.buttonWidth, self.buttonHeight)
+    love.graphics.setColor(1, 1, 0, 1)
+    love.graphics.rectangle("fill", self.quitButtonX, self.buttonY + 70, self.buttonWidth, self.buttonHeight)
+    love.graphics.setColor(0, 0, 0, 1)
     love.graphics.print("Quit", self.quitButtonX + 54, self.buttonY + 82)
+    love.graphics.setColor(1, 1, 1, 1)
 end
 
 return menu
